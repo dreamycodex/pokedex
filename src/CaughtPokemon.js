41e73a7416d69a90466c0a0a22a7b30fbfc9a385
dreamycodex <dreuphoricme@gmail.com>
2020-08-01 14:27:08 +0100

    Completed Ex C and D

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CaughtPokemon = (props) => {
   console.log(props);
-  return <p>Caught 0 Pokemon on {props.date}</p>;
+  const [totalCaught, setTotalCaught] = useState(0);
+
+  function incrementTotal() {
+    setTotalCaught(totalCaught + 1);
+  }
+  return (
+    <div>
+      <p>
+        Caught {totalCaught} Pokemon on {props.date}
+        <button onClick={incrementTotal}>Add Pokemon</button>
+      </p>
+    </div>
+  );
 };
 
 export default CaughtPokemon;
